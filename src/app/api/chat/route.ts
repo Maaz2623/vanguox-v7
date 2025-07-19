df0b6cc1ef8395c108374870d7a04f4e1b6a2454
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-07-19 20:20:59 +0530

    modified:   src/app/api/chat/route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -4,6 +4,9 @@ import { SYSTEM_PROMPT } from '@/prompt';
 import { google } from '@ai-sdk/google';
 import { appendResponseMessages, streamText, smoothStream } from 'ai';
 
+
+export const runtime = 'edge'; // 👈 This tells Next.js to use Edge Runtime
+
 export async function POST(req: Request) {
   const { messages, id } = await req.json();
 
